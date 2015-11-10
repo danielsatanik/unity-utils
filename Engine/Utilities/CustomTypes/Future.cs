6108be135521ad35d8985c3b2f826f161997ea89
Daniel Satanik <daniel@satanik.at>
2015-11-10 09:49:15 +0100

    Added better async handling in Unity3d
    Idea is similar to futures from other languages like C++
    One obtains a Future<T> with an expected genric type by using the MonoBehaviour
    extension method Async(IEnumerator).

---
+++ Engine/Utilities/CustomTypes/Future.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+namespace UnityUtils.Engine.Utilities.CustomTypes
+{
+    public class FuturePassable
+    {
+        public MonoBehaviour MonoBehaviour { get; set; }
+
+        public IEnumerator Coroutine { get; set; }
+    }
+
+    public class Future<T>
+    {
+        T m_returnVal;
+        bool m_unset = true;
+        Exception m_exception;
+        Coroutine m_coroutine;
+
+        public void Wait()
+        {
+            while (m_unset)
+            {
+            }
+        }
+
+        public T Value
+        {
+            private set
+            {
+                m_unset = false;
+                m_returnVal = value;
+            }
+            get
+            {
+                while (m_unset)
+                {
+                }
+                if (m_exception != null)
+                    throw m_exception;
+                return m_returnVal;
+            }
+        }
+
+        IEnumerator InternalRoutine(IEnumerator coroutine)
+        {
+            for (;;)
+            {
+                try
+                {
+                    if (!coroutine.MoveNext())
+                        yield break;
+                }
+                catch (Exception e)
+                {
+                    m_exception = e;
+                    yield break;
+                }
+                var yielded = coroutine.Current;
+                if (yielded != null && yielded.GetType() == typeof(T))
+                {
+                    m_returnVal = (T)yielded;
+                    yield break;
+                }
+                yield return coroutine.Current;
+            }
+        }
+
+        public static implicit operator Future<T>(FuturePassable fp)
+        {
+            var future = new Future<T>();
+            var enumerator = future.InternalRoutine(fp.Coroutine);
+            future.m_coroutine = fp.MonoBehaviour.StartCoroutine(enumerator);
+            return future;
+        }
+    }
+}
---
+++ Engine/Utilities/Extensions/MonoBehaviourExtensions.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityUtils.Engine.Utilities.CustomTypes;
+
+public static class MonoBehaviourExtensions
+{
+    public static FuturePassable Async(this MonoBehaviour mb, IEnumerator coroutine)
+    {
+        return new FuturePassable
+        {
+            MonoBehaviour = mb,
+            Coroutine = coroutine
+        };
+    }
+}
