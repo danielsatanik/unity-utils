a7fa5377c8d15f9944013a03d240233b94c22741
Daniel Satanik <dsatanik.mmt-m2014@fh-salzburg.ac.at>
2015-11-18 10:58:55 +0100

    added check for properties, whether those are auto-implemented or not

---
+++ Utilities/Extensions/PropertyInfoExtension.cs
@@ -0,0 +1,37 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Linq;
+
+namespace UnityUtils.Utilities.Extensions
+{
+    public static class PropertyInfoExtension
+    {
+        public static bool IsAutoImplemented(this PropertyInfo prop)
+        {
+            bool mightBe = prop.GetGetMethod()
+                .GetCustomAttributes(
+                               typeof(CompilerGeneratedAttribute),
+                               true
+                           )
+                .Any();
+            if (!mightBe)
+            {
+                return false;
+            }
+
+
+            bool maybe = prop.DeclaringType
+                .GetFields(BindingFlags.NonPublic | BindingFlags.Instance)
+                .Where(f => f.Name.Contains(prop.Name))
+                .Where(f => f.Name.Contains("BackingField"))
+                .Any(
+                             f => f.GetCustomAttributes(
+                                 typeof(CompilerGeneratedAttribute),
+                                 true
+                             ).Any()
+                         );
+
+            return maybe;
+        }
+    }
+}
