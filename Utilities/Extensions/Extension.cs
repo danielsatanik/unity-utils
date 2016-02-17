ce64fdcb57f2b82155c290fd70c8f0df86ced15f
Daniel Satanik <daniel@satanik.at>
2016-02-17 14:16:47 +0100

    added some type extensions

--- Utilities/Extensions/Extension.cs
+++ Utilities/Extensions/Extension.cs
@@ -25,6 +25,12 @@ namespace UnityUtils.Utilities.Extensions
             return typeof(T).GetMembers(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
         }
 
+        public static object Call<T>(this T obj, System.Type type, string name, System.Type[] parameterTypes, object[] parameters)
+        {
+            var method = type.GetMethod(name, BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance, null, parameterTypes, null);
+            return method.Invoke(obj, parameters);
+        }
+
         public static object CallGeneric<T>(this T obj, string name, System.Type type, System.Type[] parameterTypes, object[] parameters) where T : class
         {
             var method = typeof(T).GetMethod(name, BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance, null, parameterTypes, null);
@@ -37,6 +43,13 @@ namespace UnityUtils.Utilities.Extensions
             var method = typeof(T).GetMethod(name, BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static, null, parameterTypes, null);
             var generic = method.MakeGenericMethod(type);
             return generic.Invoke(null, parameters);
+        }
+
+        public static object CallStaticExtension<T>(this T obj, System.Type extendingType, string name, System.Type type, System.Type[] parameterTypes, object[] parameters) where T : class
+        {
+            var method = extendingType.GetMethod(name, BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static, null, parameterTypes, null);
+            var generic = method.MakeGenericMethod(type);
+            return generic.Invoke(obj, parameters);
         }
     }
 }
---
+++ Utilities/Extensions/TypeExtension.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+
+namespace UnityUtils.Utilities.Extensions
+{
+    public static class TypeExtension
+    {
+        public static System.Collections.Generic.IEnumerable<System.Type> GetParentTypes(this System.Type type)
+        {
+            if (type.BaseType == null)
+                return type.GetInterfaces();
+
+            return Enumerable.Repeat(type.BaseType, 1)
+                .Concat(type.GetInterfaces())
+                .Concat(type.GetInterfaces().SelectMany<System.Type, System.Type>(GetParentTypes))
+                .Concat(type.BaseType.GetParentTypes());
+        }
+    }
+}
