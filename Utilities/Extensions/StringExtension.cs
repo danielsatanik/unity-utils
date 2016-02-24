5fdac2ccea5bc718e537f1b286f26bb6cd5a9a4c
Daniel Satanik <daniel@satanik.at>
2016-02-24 09:11:36 +0100

    added toplural for strings

--- Utilities/Extensions/StringExtension.cs
+++ Utilities/Extensions/StringExtension.cs
@@ -25,6 +25,33 @@ namespace UnityUtils.Utilities.Extensions
             return ToDelimiterCase(str, "_");
         }
 
+        public static string ToPlural(this string singular)
+        {
+            // Multiple words in the form A of B : Apply the plural to the first word only (A)
+            int index = singular.LastIndexOf(" of ");
+            if (index > 0)
+                return (singular.Substring(0, index)) + singular.Remove(0, index).ToPlural();
+
+            // single Word rules
+            //sibilant ending rule
+            if (singular.EndsWith("sh"))
+                return singular + "es";
+            if (singular.EndsWith("ch"))
+                return singular + "es";
+            if (singular.EndsWith("us"))
+                return singular + "es";
+            if (singular.EndsWith("ss"))
+                return singular + "es";
+            //-ies rule
+            if (singular.EndsWith("y"))
+                return singular.Remove(singular.Length - 1, 1) + "ies";
+            // -oes rule
+            if (singular.EndsWith("o"))
+                return singular.Remove(singular.Length - 1, 1) + "oes";
+            // -s suffix rule
+            return singular + "s";
+        }
+
         public static bool IEquals(this string str1, string str2)
         {
             return string.Equals(str1, str2, StringComparison.CurrentCultureIgnoreCase);
